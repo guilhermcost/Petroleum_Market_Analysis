906f6432b7ca05923d9de3cf50469f01d09e6bc6
Guilherme_GitHub <guilhermecost97@gmail.com>
2023-12-11 11:29:14 -0300

    upload datasets and config files

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+abril_data = pd.read_csv('datasets/abril.csv', sep = ';', encoding= 'latin-1') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+     
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    
+    # criar um histograma
+    fig = px.histogram(abril_data, x='OPERADOR')
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+
+# criar uma caixa de seleção
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram: # se a caixa de seleção for selecionada
+  st.write('Criando um histograma para a coluna odometer')
