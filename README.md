a1dbadd29598e6bc008b4a57e0c043867c4c1395
Guilherme_GitHub <guilhermecost97@gmail.com>
2023-12-11 15:55:27 -0300

    update app.py, README.md and EDA.ipynb files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Petroleum_Market_Analysis
-Dashboard de aplicativo web em um serviço de nuvem que contém uma breve análise sobre o mercado de petróleo e suas projeções durante a próxima década.
+Dashboard de aplicativo web em um serviço de nuvem que contém uma breve análise sobre o mercado de petróleo e os poços exploratórios registrados até 2023.
+
+# Objeto de Estudo
+Dados gerais sobre os poços exploratórios que tiveram a perfuração iniciada a partir do ano de 1998 até abril/2023
+
+# Descrição dos Dados
+
+Disponível no arquivo _"metadado-pocos-exploratorios.pdf"_ ou no Link:https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/fase-exploracao.
+
+## Resultados
+
+Os resultados dessa análise foram armazenados em nuvem através da aplicação *Render* e são apresentados em: https://marketanalysis-petroleum.onrender.com
--- app.py
+++ app.py
@@ -2,21 +2,65 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-abril_data = pd.read_csv('datasets/abril.csv', sep = ';', encoding= 'latin-1') # lendo os dados
-hist_button = st.button('Criar histograma') # criar um botão
-     
+oil_data = pd.read_csv('datasets/abril.csv', sep = ';', encoding= 'latin-1') # lendo os dados
+
+## --------------------------------------------------------
+## --------------- LIMPEZA DOS DADOS ----------------------
+## --------------------------------------------------------
+
+## Passo 1: Descarte da coluna "DATA DE NOTIFICAÇÃO DE DESCOBERTA", pois ela não será utilizada nesta análise
+oil_data.drop(oil_data.columns[-1], axis = 1, inplace = True)
+
+## Passo 2: Renomear colunas que possuem acentoes, espaços e cedilhas
+oil_data.columns = ['Poco_ANP', 'Bloco', 'Bacia', 'Estado', 'Ambiente', 'Operador', 'Inicio', 'Conclusao', 'Notificacao']
+
+#Passo 3: Remoção de linhas com dados completamente ausentes - Linha 1730 e 1731
+oil_data.drop([1730, 1731], inplace = True)
+oil_data.reset_index(drop=True, inplace= True)
+
+#Passo 4: Substituindo valores ausentes por valor desconhecido
+oil_data['Operador'].fillna('Desconhecido', inplace = True)
+
+#Passo 5: Troca de tipos de Dados das colunas "Inicio" e "Conclusao" para Datetime
+
+oil_data['Inicio'] = pd.to_datetime(oil_data['Inicio'], format = '%d/%m/%Y')
+oil_data['Conclusao'] = pd.to_datetime(oil_data['Conclusao'], format = '%d/%m/%Y')
+
+#Passo 6: Criação da linha apenas com o ano de início da exploração de cada poço
+oil_data['Ano_Inicio'] = oil_data['Inicio'].dt.year
+
+## --------------------------------------------------------
+## ----------- FIM  DA LIMPEZA DOS DADOS ------------------
+## --------------------------------------------------------
+
+st.header("Análise da distribuição de poços de petróleo pelo Brasil de 1998 até Abril de 2023", divider = 'blue')
+
+
+hist_button = st.checkbox('Exibir Gráficos') # criar um botão
+
 if hist_button: # se o botão for clicado
+
+    ## ------- GRÁFICO 1 ----------##
+
     # escrever uma mensagem
-    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-    
+    st.write('**Gráfico 1: Número de Poços de Petróleo (Concluídos ou em Andamento) por Estado**')
+
     # criar um histograma
-    fig = px.histogram(abril_data, x='OPERADOR')
+    fig = px.histogram(oil_data, x='Estado')
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
-# criar uma caixa de seleção
-build_histogram = st.checkbox('Criar um histograma')
+    ## ------- GRÁFICO 2 ----------##
 
-if build_histogram: # se a caixa de seleção for selecionada
-  st.write('Criando um histograma para a coluna odometer')
+    ## Agrupamento de dados por ano e contagem do número de poços por ano
+    pocos_por_ano = oil_data.groupby('Ano_Inicio')['Poco_ANP'].count()
+
+    # escrever uma mensagem
+    st.write('**Gráfico 2: Número de Poços de Petróleo Iniciados por Ano**')
+
+    # criar um gráfico de dispersão
+    fig = px.scatter(x=pocos_por_ano.index, y=pocos_por_ano.values)
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
